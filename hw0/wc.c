ef873ce1af482c25350c20a3cba6e466abe7e70b
Adam <shi.adam@berkeley.edu>
2016-01-28 00:56:35 +0000

    wc check

--- hw0/wc.c
+++ hw0/wc.c
@@ -1,3 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
 int main(int argc, char *argv[]) {
-    return 0;
+	int words = 0; // counts words
+	int lines = 0; // counts lines
+	int bytes = 0; // counts bytes
+	int isNewWord = 1; // determines if the next character is part of a new word
+
+	if (argc == 1) {
+	// read from standard input until end of file character is input
+		char s[1];
+		while (fread(s, 1, 1, stdin)) {
+			bytes++;
+			if (isNewWord && !isspace(*s)) {
+				words++;
+				isNewWord = 0;
+			} else if (isspace(*s)) {
+				isNewWord = 1;
+			}
+			if (*s == '\n') {
+				lines++;
+			}
+		}
+	} else {
+	// read from a file
+
+	}
+
+	// print stuff
+	printf("      %d       %d       %d\n", lines, words, bytes);
+	return 0;
 }
