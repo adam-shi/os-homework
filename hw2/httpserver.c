5490e26d062be2dd4ee8979d01513026b5c6e319
Adam <shi.adam@berkeley.edu>
2016-02-29 03:44:41 +0000

    hard code 3 byte offset try

--- hw2/httpserver.c
+++ hw2/httpserver.c
@@ -186,8 +186,8 @@ void handle_proxy_request(int fd) {
     nfds = socket_number + 1;
   } 
 
-  lseek(fd, 0, SEEK_END);
-  lseek(socket_number, 0, SEEK_END);
+  lseek(fd, 3, SEEK_CUR);
+  lseek(socket_number, 3, SEEK_CUR);
 
   while (fcntl(fd, F_GETFD) != -1 && fcntl(socket_number, F_GETFD) != -1) {
     FD_SET(fd, &readfds);
