4ac62539e36565a408dbef86b8031ecebab1468f
Adam <shi.adam@berkeley.edu>
2016-03-13 21:29:02 +0000

    first try on malloc

--- hw3/mm_alloc.c
+++ hw3/mm_alloc.c
@@ -6,13 +6,43 @@
 
 #include "mm_alloc.h"
 #include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+struct metadata* head = NULL;
+
+struct metadata
+{
+	struct metadata* prev;
+	struct metadata* next;
+	int is_free;
+	size_t size;
+};
+
+const size_t header_size = sizeof(struct metadata);
 
 void *mm_malloc(size_t size) {
     if (size == 0) {
     	return NULL;
     }
 
-    
+    if (head == NULL) {
+    	struct metadata* heap_start = sbrk(header_size + size);
+
+    	struct metadata header;
+    	header.prev = NULL;
+    	header.next = NULL;
+    	header.is_free = 0;
+    	header.size = size;
+    	*heap_start = header;
+
+    	head = heap_start;
+
+    	memset((heap_start + header_size), 0, size);
+    	return (void*) (heap_start + header_size);
+    }
+
+
 
     /* YOUR CODE HERE */
     return NULL;
